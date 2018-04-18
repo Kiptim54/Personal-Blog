16c03c5fcf4012ef3642bedd35ccf01238fe2810
Kiptim <kiptim54@gmail.com>
2018-04-18 16:14:27 +0300

    add model user

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kiptim:jerotich@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 config_options ={"production":ProdConfig,"default":DevConfig}
 

91a0c73b9ab8b02ff91cb40e3b599f9f95a2131d
Kiptim <kiptim54@gmail.com>
2018-04-23 10:25:12 +0300

    add nothing

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 import os 
+import psycopg2
+
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kiptim:jerotich@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'kenani'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('kiptim54@gmail.com')
-    MAIL_PASSWORD = os.environ.get('jerotich')
+    MAIL_USERNAME = 'kiptim54@gmail.com'
+    MAIL_PASSWORD = 'jerotich'
     SENDER_EMAIL = 'kiptim54@gmail.com'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
      # simple mde  configurations
@@ -18,9 +20,12 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kiptim:jerotich@localhost/blog' 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # connection = psycopg2.connect(database="gps_heatmap", user="postgres", password="1234", host="localhost", port=5433)
     DEBUG = True
+
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kiptim:jerotich@localhost/blog'
     DEBUG = True
     
 

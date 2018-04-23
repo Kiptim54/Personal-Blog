25d9de0d4ed932c86e11847c2a247c4aac30495d
Kiptim <kiptim54@gmail.com>
2018-04-23 13:19:46 +0300

    remove prod

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class Config:
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kiptim:jerotich@localhost/blog'
+    
     
     DEBUG = True
 
